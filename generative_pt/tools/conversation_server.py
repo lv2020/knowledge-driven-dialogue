231b69cdc4d7488e53adba0f4a0c963510b57f85
李威 <victor@littlelights.ai>
2019-11-09 18:25:07 +0800

    okk

--- generative_pt/tools/conversation_server.py
+++ generative_pt/tools/conversation_server.py
@@ -18,31 +18,12 @@ from _thread import start_new_thread
 from tools.conversation_strategy import load
 from tools.conversation_strategy import predict
 
- 
 
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 8601
-
-print("starting conversation server ...")
-print("binding socket ...")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#Bind socket to local host and port
-try:
-    s.bind((SERVER_IP, SERVER_PORT))
-except socket.error as msg:
-    print("Bind failed. Error Code : " + str(msg[0]) + " Message " + msg[1])
-    exit()
-#Start listening on socket
-s.listen(10)
-print("bind socket success !")
-
-print("loading model...")
-model = load()
-print("load model success !")
-
-print("start conversation server success !")
 
 def main():
+    print("loading model...")
+    model = load()
+    print("load model success !")
 
     for line in open(sys.argv[1]):
         param = line.strip()
